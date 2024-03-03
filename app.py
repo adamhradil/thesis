3ecb714c1e50a781de9d4e714b84fc6aa2bd6b6c
Adam Hradil <hradilad@gmail.com>
2024-03-03 20:44:25 +0100

    acommodate bezrealitky listings

--- app.py
+++ app.py
@@ -15,14 +15,53 @@ from bezrealitky_scraper.bezrealitky.spiders.search_flats import SearchFlatsSpid
 
 # from sreality_scraper.sreality.spiders.sreality_spider import SrealitySpider
 
+class UserPreferences:
+    def __init__(
+        self,
+        dispositions=None,
+        weight_area=None,
+        weight_rent=None,
+        weight_location=None,
+        min_area=None,
+        max_area=None,
+        min_price=None,
+        max_price=None,
+        ownership_type=None,
+        state=None,
+        property_material=None,
+        furnished=None,
+        balcony=None,
+        terrace=None,
+        loggia=None,
+        cellar=None,
+        front_garden=None,
+    ) -> None:
+        self.dispositions: list[Disposition] | None = dispositions
+        self.weight_area: float | None = weight_area
+        self.weight_rent: float | None = weight_rent
+        self.weight_location: float | None = weight_location
+        self.min_area: int | None = min_area
+        self.max_area: int | None = max_area
+        self.min_price: int | None = min_price
+        self.max_price: int | None = max_price
+        self.ownership_type: list[OwnershipType] | None = ownership_type
+        self.state: list[PropertyState] | None = state
+        self.property_material: list[PropertyMaterial] | None = property_material
+        self.furnished: Furnished | None = furnished
+        self.balcony: bool | None = balcony
+        self.terrace: bool | None = terrace
+        self.loggia: bool | None = loggia
+        self.cellar: bool | None = cellar
+        self.front_garden: bool | None = front_garden
+
 
 class Listing:
     def __init__(self, data: dict):
-        self.address = data.get("address", "")
-        self.area = data.get("area", "")
-        self.available_from = data.get("available_from", "")
-        self.description = data.get("description", "")
-        self.disposition = data.get("disposition", "")
+        self.address: str = data.get("address", "")
+        self.area: int = data.get("area", "")
+        self.available_from: str = data.get("available_from", "")
+        self.description: str = data.get("description", "")
+        self.disposition: str = data.get("disposition", "")
         self.floor = data.get("floor", "")
         self.furnished = data.get("furnished", "")
         self.id = data.get("id", "")
@@ -33,6 +72,16 @@ class Listing:
         self.status = data.get("status", "")
         self.type = data.get("type", "")
         self.url = data.get("url", "")
+        self.balcony = data.get("balcony", "")
+        self.cellar = data.get("cellar", "")
+        self.front_garden = data.get("front_garden", "")
+        self.terrace = data.get("terrace", "")
+        self.elevator = data.get("elevator", "")
+        self.parking = data.get("parking", "")
+        self.garage = data.get("garage", "")
+        self.pets = data.get("pets", "")
+        self.loggie = data.get("loggie", "")
+        self.public_transport = data.get("public_transport", "")
 
     def __eq__(self, other):
         if isinstance(other, Listing):
@@ -53,50 +102,55 @@ class Listing:
                 Service Fees: {self.service_fees}
                 Status: {self.status}
                 Type: {self.type}
-                URL: {self.url}"""
+                URL: {self.url}
+                Balcony: {self.balcony}
+                Cellar: {self.cellar}
+                Front Garden: {self.front_garden}
+                Terrace: {self.terrace}
+                Elevator: {self.elevator}
+                Parking: {self.parking}
+                Garage: {self.garage}
+                Pets: {self.pets}
+                Loggie: {self.loggie}
+                Public Transport: {self.public_transport}
+                """
 
-    def calculate_score(self, user_preferences):
+
+
+    def is_relevant(self, user_preferences: UserPreferences):
+        if user_preferences.dispositions and self.disposition not in user_preferences.dispositions:
+            return False
+        if user_preferences.min_area and self.area < user_preferences.min_area:
+            return False
+        if user_preferences.max_area and self.area > user_preferences.max_area:
+            return False
+        if user_preferences.min_price and self.rent < user_preferences.min_price:
+            return False
+        if user_preferences.max_price and self.rent > user_preferences.max_price:
+            return False
+        if user_preferences.ownership_type and self.type not in user_preferences.ownership_type:
+            return False
+        if user_preferences.state and self.status not in user_preferences.state:
+            return False
+        if user_preferences.property_material and self.penb not in user_preferences.property_material:
+            return False
+        if user_preferences.furnished and self.furnished != user_preferences.furnished:
+            return False
+        if user_preferences.balcony is not None and self.balcony != user_preferences.balcony:
+            return False
+        if user_preferences.terrace is not None and self.terrace != user_preferences.terrace:
+            return False
+        if user_preferences.loggia is not None and self.loggie != user_preferences.loggia:
+            return False
+        if user_preferences.cellar is not None and self.cellar != user_preferences.cellar:
+            return False
+        if user_preferences.front_garden is not None and self.front_garden != user_preferences.front_garden:
+            return False
+        return True
+
+
+    def calculate_score(self, user_preferences: UserPreferences):
         pass
-
-
-class UserPreferences:
-    def __init__(
-        self,
-        dispositions=None,
-        weight_area=None,
-        weight_rent=None,
-        weight_location=None,
-        min_area=None,
-        max_area=None,
-        min_price=None,
-        max_price=None,
-        ownership_type=None,
-        state=None,
-        property_material=None,
-        furnished=None,
-        balcony=None,
-        terrace=None,
-        loggia=None,
-        cellar=None,
-        garden=None,
-    ) -> None:
-        self.dispositions: list[Disposition] = dispositions
-        self.weight_area: float = weight_area
-        self.weight_rent: float = weight_rent
-        self.weight_location: float = weight_location
-        self.min_area: int = min_area
-        self.max_area: int = max_area
-        self.min_price: int = min_price
-        self.max_price: int = max_price
-        self.ownership_type: list[OwnershipType] = ownership_type
-        self.state: list[PropertyState] = state
-        self.property_material: list[PropertyMaterial] = property_material
-        self.furnished: Furnished = furnished
-        self.balcony: bool = balcony
-        self.terrace: bool = terrace
-        self.loggia: bool = loggia
-        self.cellar: bool = cellar
-        self.garden: bool = garden
 
 
 class Disposition(Enum):
@@ -195,6 +249,24 @@ if __name__ == "__main__":
         with open(FILE, "r", encoding="utf-8") as f:
             items = json.load(f)
 
+    listings = []
+    for i in items:
+        listings.append(Listing(i))
+
     dist = calculate_distance(POI, items[0]["address"])
+
+    preferences = UserPreferences(
+        dispositions=[
+            Disposition.TWO_PLUS_ONE,
+            Disposition.THREE_PLUS_KK,
+            Disposition.THREE_PLUS_ONE,
+        ],
+        weight_area=0.5,
+        weight_rent=0.4,
+        weight_location=0.1,
+        min_area=50,
+        max_price=30000,
+        balcony=True,
+    )
 
     print("test")
