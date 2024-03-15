47444bd9d4e35d5daa29d357707eec9a819ef49f
Adam Hradil <hradilad@gmail.com>
2024-03-15 18:36:57 +0100

    create listings table dynamically

--- database_wrapper.py
+++ database_wrapper.py
@@ -16,98 +16,24 @@ class DatabaseWrapper:
         """create a table from the create_table_sql statement"""
         try:
             c = self.conn.cursor()
-            c.execute(
-                """
-                CREATE TABLE IF NOT EXISTS listings (
-                    id text PRIMARY KEY,
-                    address text,
-                    area integer,
-                    available_from text,
-                    description text,
-                    disposition text,
-                    floor text,
-                    furnished text,
-                    rent text,
-                    security_deposit text,
-                    service_fees text,
-                    status text,
-                    type text,
-                    url text,
-                    balcony text,
-                    cellar text,
-                    front_garden text,
-                    terrace text,
-                    elevator text,
-                    parking text,
-                    garage text,
-                    pets text,
-                    loggie text,
-                    public_transport text,
-                    gps_lat text,
-                    gps_lon text,
-                    date_created text,
-                    date_updated text
-                );
-            """
-            )
+            listing = Listing()
+            columns = [attr for attr in dir(listing) if not callable(getattr(listing, attr)) and not attr.startswith("__")]
+            create_table_sql = f"CREATE TABLE IF NOT EXISTS listings ({','.join(columns)});"
+            c.execute(create_table_sql)
         except Error as e:
             print(e)
 
-    def verify_table_columns(self):
-        """
-        Verify that the table columns are correct
-        """
-        c = self.conn.cursor()
-        c.execute("PRAGMA table_info(listings)")
-        columns = c.fetchall()
-        if len(columns) != 25:
-            return False
-        return True
 
     def insert_listing(self, listing, date_created):
         """
         Insert a Listing into the listings table
         """
-        sql = """ INSERT INTO listings(id,address,area,available_from,
-                  description,disposition,floor,furnished,rent,
-                  security_deposit,service_fees,status,type,url,
-                  balcony,cellar,front_garden,terrace,elevator,
-                  parking,garage,pets,loggie,public_transport,gps_lat,gps_lon,date_created,date_updated)
-                  VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) """
+
+        columns = [attr for attr in dir(listing) if not callable(getattr(listing, attr)) and not attr.startswith("__")]
+        placeholders = ','.join(['?' for _ in columns])
+        sql = f"INSERT INTO listings({','.join(columns)}) VALUES({placeholders}) "
         cur = self.conn.cursor()
-        cur.execute(
-            sql,
-            (
-                listing.id,
-                listing.address,
-                listing.area,
-                listing.available_from,
-                listing.description,
-                listing.disposition,
-                listing.floor,
-                listing.furnished,
-                listing.rent,
-                listing.security_deposit,
-                listing.service_fees,
-                listing.status,
-                listing.type,
-                listing.url,
-                listing.balcony,
-                listing.cellar,
-                listing.front_garden,
-                listing.terrace,
-                listing.elevator,
-                listing.parking,
-                listing.garage,
-                listing.pets,
-                listing.loggie,
-                listing.public_transport,
-                listing.gps[0],
-                listing.gps[1],
-                date_created,
-                None,
-            ),
-        )
+        cur.execute(sql, [getattr(listing, attr) for attr in columns])
         self.conn.commit()
         return cur.lastrowid
 
@@ -139,68 +65,12 @@ class DatabaseWrapper:
         """
         Update a Listing in the listings table
         """
-        sql = """ UPDATE listings
-                  SET address = ?,
-                      area = ?,
-                      available_from = ?,
-                      description = ?,
-                      disposition = ?,
-                      floor = ?,
-                      furnished = ?,
-                      rent = ?,
-                      security_deposit = ?,
-                      service_fees = ?,
-                      status = ?,
-                      type = ?,
-                      url = ?,
-                      balcony = ?,
-                      cellar = ?,
-                      front_garden = ?,
-                      terrace = ?,
-                      elevator = ?,
-                      parking = ?,
-                      garage = ?,
-                      pets = ?,
-                      loggie = ?,
-                      public_transport = ?,
-                      gps_lat = ?
-                      gps_lon = ?
-                      date_created = ?
-                      date_updated = ?
-                  WHERE id = ?"""
+        attributes = [attr for attr in dir(listing) if not callable(getattr(listing, attr)) and not attr.startswith('__')]
+        sql = f"UPDATE listings SET {','.join([f'{attr}=?' for attr in attributes if attr != 'id'])} WHERE id=?"
         cur = self.conn.cursor()
-        cur.execute(
-            sql,
-            (
-                listing.address,
-                listing.area,
-                listing.available_from,
-                listing.description,
-                listing.disposition,
-                listing.floor,
-                listing.furnished,
-                listing.rent,
-                listing.security_deposit,
-                listing.service_fees,
-                listing.status,
-                listing.type,
-                listing.url,
-                listing.balcony,
-                listing.cellar,
-                listing.front_garden,
-                listing.terrace,
-                listing.elevator,
-                listing.parking,
-                listing.garage,
-                listing.pets,
-                listing.loggie,
-                listing.public_transport,
-                listing.gps[0],
-                listing.gps[1],
-                listing.date_created,
-                date_updated,
-            ),
-        )
+        attribute_values = [getattr(listing, attr) for attr in attributes if attr != 'id']
+        attribute_values.append(listing.id)
+        cur.execute(sql, attribute_values)
         self.conn.commit()
         
     def get_df(self):
